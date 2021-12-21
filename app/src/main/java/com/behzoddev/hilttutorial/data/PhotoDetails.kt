177f2201c30d71be421c8806c4030c22c36a0e9f
behzod <behzoddev@gmail.com>
2021-12-21 10:11:31 +0900

    Add data model classes

---
+++ app/src/main/java/com/behzoddev/hilttutorial/data/PhotoDetails.kt
@@ -0,0 +1,19 @@
+package com.behzoddev.hilttutorial.data
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class PhotoDetails(
+    val url: String?,
+    val description: String?,
+    val votesCount: Int?,
+    val comments: Int?,
+    val pulse: Double?,
+    val impressions: Int?,
+    val photoTitle: String?,
+    val userName: String?,
+    val userPhotoUrl: String?,
+    val exif: String?,
+    val durationPosted: String?
+) : Parcelable
