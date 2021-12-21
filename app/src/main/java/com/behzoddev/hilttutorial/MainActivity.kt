6e1f7608397bc75190c4687dcefb7e621f8ee01d
behzod <behzoddev@gmail.com>
2021-12-21 15:15:16 +0900

    Update service and main

--- app/src/main/java/com/behzoddev/hilttutorial/MainActivity.kt
+++ app/src/main/java/com/behzoddev/hilttutorial/MainActivity.kt
@@ -2,12 +2,24 @@ package com.behzoddev.hilttutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.behzoddev.hilttutorial.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host) as NavHostFragment?
+            ?: return
     }
 }
