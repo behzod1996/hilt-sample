67a25944374fe1a7989cca06ead16cdc1044e3bc
behzod <behzoddev@gmail.com>
2021-12-21 15:16:09 +0900

    Update viewModel and details

--- app/src/main/java/com/behzoddev/hilttutorial/view/detail/DetailFragment.kt
+++ app/src/main/java/com/behzoddev/hilttutorial/view/detail/DetailFragment.kt
@@ -1,18 +1,59 @@
 package com.behzoddev.hilttutorial.view.detail
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.viewbinding.ViewBinding
+import com.behzoddev.hilttutorial.common.loadImage
+import com.behzoddev.hilttutorial.common.loadUrl
+import com.behzoddev.hilttutorial.common.toastLong
+import com.behzoddev.hilttutorial.common.toastShort
+import com.behzoddev.hilttutorial.data.Photo
+import com.behzoddev.hilttutorial.data.PhotoDetails
 import com.behzoddev.hilttutorial.databinding.FragmentDetailBinding
+import com.behzoddev.hilttutorial.util.Resource
 import com.behzoddev.hilttutorial.view.base.BaseFragment
+import com.behzoddev.hilttutorial.view.viewmodel.PhotosViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DetailFragment : BaseFragment<FragmentDetailBinding>() {
+
+    private val viewModel : PhotosViewModel by activityViewModels()
+
     override fun getViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
-    ): FragmentDetailBinding {
-        return FragmentDetailBinding.inflate(inflater,container,false)
+    ): FragmentDetailBinding =
+        FragmentDetailBinding.inflate(inflater,container,false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.let {
+            viewModel.processPhotoDetailsArgument(it)
+                .observe(viewLifecycleOwner, Observer { result ->
+                    when(result) {
+                        is Resource.OnSuccess -> bind(result.data)
+                        is Resource.OnFailure -> context?.toastLong(result.throwable.message!!)
+                    }
+                })
+        } ?: context?.toastLong("Invalid data")
     }
+
+
+    private fun bind(photoDetails: PhotoDetails) = with(binding) {
+        ivPhotoDetails.loadImage(photoDetails.url)
+        tvLikesValue.text = photoDetails.votesCount.toString()
+        tvCommentsValue.text = photoDetails.comments.toString()
+        tvDescriptionValue.text = photoDetails.description.toString()
+        tvPostedValue.text = photoDetails.durationPosted.toString()
+    }
+
 
 }
--- app/src/main/java/com/behzoddev/hilttutorial/view/viewmodel/PhotosViewModel.kt
+++ app/src/main/java/com/behzoddev/hilttutorial/view/viewmodel/PhotosViewModel.kt
@@ -1,14 +1,27 @@
 package com.behzoddev.hilttutorial.view.viewmodel
 
+import android.os.Bundle
+import androidx.annotation.NonNull
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.behzoddev.hilttutorial.data.Photo
+import com.behzoddev.hilttutorial.data.PhotoDetails
+import com.behzoddev.hilttutorial.data.PhotoResponse
 import com.behzoddev.hilttutorial.repository.PhotosRepositoryImpl
 import com.behzoddev.hilttutorial.util.Resource
+import com.behzoddev.hilttutorial.util.getViewStateFlowForNetworkCall
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import java.lang.Exception
+import javax.inject.Inject
 
 @HiltViewModel
-class PhotosViewModel constructor(private val repo: PhotosRepositoryImpl) : ViewModel() {
+class PhotosViewModel @Inject constructor(private val repo: PhotosRepositoryImpl) : ViewModel() {
 
     val popularPhotosLiveData: MutableLiveData<Resource<List<Photo>>> by lazy {
         MutableLiveData<Resource<List<Photo>>>()
@@ -17,5 +30,52 @@ class PhotosViewModel constructor(private val repo: PhotosRepositoryImpl) : View
     private var currentPageNumber = 1
     private var maximumPageNumber = 2
     private val photoList = ArrayList<Photo>()
+
+    private var navigatingFromDetails = false
+
+
+    @ExperimentalCoroutinesApi
+    fun getPhotosNextPage() {
+        if (navigatingFromDetails) {
+            popularPhotosLiveData.value = Resource.OnSuccess(photoList)
+            return
+        }
+
+        if(currentPageNumber<maximumPageNumber) {
+            viewModelScope.launch {
+                getViewStateFlowForNetworkCall {
+                    repo.getPhotosByPage(currentPageNumber)
+                }.collect {
+                    when(it) {
+                        is Resource.OnLoading -> popularPhotosLiveData.value = it
+                        is Resource.OnFailure -> popularPhotosLiveData.value = it
+                        is Resource.OnSuccess<PhotoResponse> -> {
+                            currentPageNumber++
+                            maximumPageNumber = it.data.totalPages
+                            photoList.addAll(it.data.photos)
+                            popularPhotosLiveData.value = Resource.OnSuccess(photoList)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun processPhotoDetailsArgument(@NonNull args: Bundle) =
+        flow {
+            val photoDetails = args.getParcelable<PhotoDetails>("photoDetails")
+
+            photoDetails?.let {
+                emit(Resource. OnSuccess(it))
+            } ?: run {
+                emit(Resource.OnFailure(Exception("No Photo DetailsFound")))
+        }
+    }.asLiveData()
+
+    @ExperimentalCoroutinesApi
+    fun onRecyclerViewScrolledToBottom() {
+        if(navigatingFromDetails) navigatingFromDetails = false
+        getPhotosNextPage()
+    }
 
 }
